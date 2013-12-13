c4784db906147fdc02a39f898c3ceb3e4ec003ad
Pat George <pat.george@hungrymachine.com>
2013-12-13 09:58:08 -0500

    Added Summoner#names api call.

--- lib/riot_api/resource/summoner.rb
+++ lib/riot_api/resource/summoner.rb
@@ -10,6 +10,13 @@ module RiotApi
         RiotApi::Model::Summoner.new @connection.get("#{base_path}/#{id}/").body
       end
 
+      def names(*ids)
+        ids = ids.compact.join(',')
+        @connection.get("#{base_path}/#{ids}/name").body.summoners.map do |summoner|
+          RiotApi::Model::Summoner.new summoner
+        end
+      end
+
       private
 
       def base_path
