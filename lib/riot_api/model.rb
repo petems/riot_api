686313b46cde55e0f85cc0c139f4b61c14b8e684
Pat George <pat.george@gmail.com>
2013-12-16 10:53:23 -0500

    Added Game model.

--- lib/riot_api/model.rb
+++ lib/riot_api/model.rb
@@ -1,7 +1,8 @@
 require 'riot_api/model/base'
-require 'riot_api/model/summoner'
+require 'riot_api/model/game'
 require 'riot_api/model/champion'
 require 'riot_api/model/rune'
+require 'riot_api/model/summoner'
 
 module RiotApi
   module Model
---
+++ lib/riot_api/model/game.rb
@@ -0,0 +1,19 @@
+module RiotApi
+  module Model
+    class Game < Base
+      attribute :champion_id, Integer
+      attribute :create_date, String
+      attribute :create_date_str, String
+      attribute :fellow_players, Array[Hash]
+      attribute :game_id, Integer
+      attribute :game_mode, String
+      attribute :level, Integer
+      attribute :map_id, Integer
+      attribute :spell1, Integer
+      attribute :spell2, Integer
+      attribute :statistics, Array[Hash]
+      attribute :sub_type, String
+      attribute :team_id, Integer
+    end
+  end
+end
