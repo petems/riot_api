c3d9162cd094069cb4a46d6cf7c09fe13494ae73
Pat George <pat.george@gmail.com>
2013-12-19 09:18:32 -0500

    Implemented classes to support masteries call.

--- lib/riot_api/model.rb
+++ lib/riot_api/model.rb
@@ -2,6 +2,7 @@ require 'riot_api/model/base'
 require 'riot_api/model/champion'
 require 'riot_api/model/game'
 require 'riot_api/model/league'
+require 'riot_api/model/mastery_page'
 require 'riot_api/model/rune_page'
 require 'riot_api/model/summoner'
 
